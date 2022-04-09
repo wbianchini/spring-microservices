fbe1336c2762b93e853b2148e751a9f3ab38bcb9
William Bianchini <william.bianchini@gmail.com>
2022-04-09 13:14:43 -0300

    server discovery with eureka-server

--- customer/src/main/java/com/amigosservice/customer/CustomerService.java
+++ customer/src/main/java/com/amigosservice/customer/CustomerService.java
@@ -3,8 +3,6 @@ package com.amigosservice.customer;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @Service
 public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
     public void registerCustomer(CustomerRegistrationRequest request) {
@@ -16,7 +14,7 @@ public record CustomerService(CustomerRepository customerRepository, RestTemplat
          customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse  fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
--- fraud/src/main/java/com/amigosservice/fraud/FraudController.java
+++ fraud/src/main/java/com/amigosservice/fraud/FraudController.java
@@ -1,8 +1,10 @@
 package com.amigosservice.fraud;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
